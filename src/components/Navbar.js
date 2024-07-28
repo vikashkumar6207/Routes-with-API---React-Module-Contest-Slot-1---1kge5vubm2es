ed0651de95adb4ccb82686bc10b404479e617674
vikashkumar6207 <136398175+vikashkumar6207@users.noreply.github.com>
2024-07-28 14:21:06 +0530

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className='nav'>
-        <Link className='site-title'>Site Name</Link>
+        <Link className='site-title' to="/">Site Name</Link>
         <ul>
             <li>
-                <Link className='top-ten'>Top 10</Link>
+                <Link className='top-ten' to="/top-10">Top 10</Link>
             </li>
             <li>
-                <Link className='contact'>Contact</Link>
+                <Link className='contact' to="/contact">Contact</Link>
             </li>
         </ul>
     </nav>
