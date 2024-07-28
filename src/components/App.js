cce305cb1da90c94269bfcf30d13da901bc3511d
vikashkumar6207 <136398175+vikashkumar6207@users.noreply.github.com>
2024-07-28 14:28:15 +0530

    Update App

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,15 @@ const App = () => {
 
   return (
     <div id="main">
+    
       <Navbar />
-      <div className='container'></div>
+      <div className='container'>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/top-10" element={<Top />} />
+    <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </div>
     </div>
   )
 }
